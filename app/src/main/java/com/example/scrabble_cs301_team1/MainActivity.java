7ababa4fbb95fe91d08f340c32a11f03793af8b5
samonewatkins <watkinss23@up.edu>
2021-02-28 18:50:11 -0800

    Initial commit

---
+++ app/src/main/java/com/example/scrabble_cs301_team1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.scrabble_cs301_team1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
